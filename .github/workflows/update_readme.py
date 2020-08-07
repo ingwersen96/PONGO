8dd3ff9377d43250cf8fe162256323436df48449
ingwersen96 <35817014+ingwersen96@users.noreply.github.com>
2020-08-07 20:54:31 -0300

    Update and rename test.py to update_readme.py

---
+++ .github/workflows/update_readme.py
@@ -0,0 +1,21 @@
+import os
+from datetime import datetime
+import sys
+
+def main(sonar_cloud_project_name):
+    
+    readme = "./readme.md"
+    
+    current_date = datetime.now()
+    
+    sonar_cloud_url = "https://sonarcloud.io/dashboard?id="
+    sonar_report_url = sonar_cloud_url + sonar_cloud_project_name
+    
+    data = "* Last Repository check was made at: {} \n Check code analysis results at:{}".format(current_date, sonar_report_url)
+
+    with open(readme, "a") as f:
+        print(data, file=f)
+
+if __name__ == "__main__":
+    main(sys.argv)
+    
